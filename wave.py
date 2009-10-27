28c0365fe452f243f5b37c0856876a86c1b5949a
Scott Ferguson <sferguson@quikorder.com>
2009-10-27 15:25:04 -0500

    Back to GTK

--- wave.py
+++ wave.py
@@ -1,5 +1,9 @@
-import wx
-
+import pygtk
+pygtk.require("2.0")
+import gtk
+import gtk.glade
+import gobject
+ 
 try:
     import pynotify
 
@@ -15,8 +19,10 @@ except ImportError:
 import threading
 import time
 import os
-
+ 
 import waveNotifier
+ 
+gobject.threads_init()
 
 class WaveCheckThread(threading.Thread):
     def __init__(self, username, password):
@@ -45,79 +51,64 @@ class WaveCheckThread(threading.Thread):
                 PySnarl.snShowMessage('Google Wave', 'You have %s unread blips' % (result[1]), 5, iconPath = os.getcwd() + '\wave_logo.png')
             except NameError:
                 print 'Unread blips: %s' % result[1]
-
+ 
 class WaveChecker:
-    def onLoginClick(self, event):
-        self.username = self.txtUsername.GetValue()
-        self.password = self.txtPassword.GetValue()
-
+    def makeToast(self, total, unread):
+        '''
+        Display a notification to the user with the number of unread blips
+        '''
+        pass
+ 
+    def onLoginClicked(self, widget, data = None):
+        self.username = self.txtUsername.get_text()
+        self.password = self.txtPassword.get_text()
+ 
         self.thread = WaveCheckThread(self.username, self.password).start()
-        self.frame.Show(False)
-
-    def onQuitClicked(self, event):
-        if event.GetId() == wx.ID_EXIT:
-            self.app.ExitMainLoop()
-
-    def onIconClick(self, event):
-        if self.frame.IsShown():
-            self.frame.Show(False)
+        self.window.hide()
+ 
+    def onIconClick(self, widget, data = None):
+        if self.window.get_property('visible'):
+            self.window.hide()
         else:
-            self.frame.Show(True)
-
-    def showPopupMenu(self, event):
-        menu = wx.Menu()
-
-        if (wx.Platform == '__WXMAC__'):
-            wx.App.SetMacExitMenuItemId(wx.ID_EXIT)
-        else:
-            exitMenuItem = menu.Append(wx.ID_EXIT, 'Quit')
-
-        wx.EVT_MENU(menu, wx.ID_EXIT, self.onQuitClicked)
-
-        self.statusIcon.PopupMenu(menu)
-
+            self.window.show()
+ 
+    def onWindowClose(self, widget, data = None):
+        self.window.hide()
+ 
+    def showPopupMenu(self, widget, button, time, data = None): 
+        if button == 3: 
+            if data: 
+                data.show_all() 
+                data.popup(None, None, None, 3, time) 
+ 
     def __init__(self):
         self.username = None
         self.password = None
-
-        self.app = wx.PySimpleApp()
-
-        # Build the login window
-        self.frame = wx.Frame(None, wx.ID_ANY, 'Google Wave Checker')
-
-        vertical = wx.BoxSizer(wx.VERTICAL)
-        horizontal = wx.BoxSizer(wx.HORIZONTAL)
-
-        userLabel = wx.StaticText(self.frame, 1, 'Username:')
-        self.txtUsername = wx.TextCtrl(self.frame, 3)
-        horizontal.Add(userLabel, 0, wx.EXPAND)
-        horizontal.Add(self.txtUsername, 1, wx.EXPAND)
-        vertical.Add(horizontal, 0, wx.EXPAND)
-        
-        passwordLabel = wx.StaticText(self.frame, 2, 'Password:')
-        self.txtPassword = wx.TextCtrl(self.frame, 4, style = wx.TE_PASSWORD)
-        horizontal.Add(passwordLabel, 0, wx.EXPAND)
-        horizontal.Add(self.txtPassword, 1, wx.EXPAND)
-        vertical.Add(horizontal, 1, wx.EXPAND)
-
-        self.btnLogin = wx.Button(self.frame, 5, 'Login')
-        vertical.Add(self.btnLogin, 2, wx.EXPAND)
-
-        self.frame.SetSizer(vertical)
-        self.frame.SetAutoLayout(1)
-        vertical.Fit(self.frame)
-
-        # Create the taskbar icon
-        icon = wx.Icon('wave_logo_taskbar.png', wx.BITMAP_TYPE_PNG)
-        self.statusIcon = wx.TaskBarIcon()
-        self.statusIcon.SetIcon(icon, 'Wave Checker')
-
-        # Bind events
-        wx.EVT_TASKBAR_RIGHT_UP(self.statusIcon, self.showPopupMenu)
-        wx.EVT_TASKBAR_LEFT_UP(self.statusIcon, self.onIconClick)
-        wx.EVT_BUTTON(self.btnLogin, 5, self.onLoginClick)
-
-        self.app.MainLoop()
-
+ 
+        glade = gtk.glade.XML('waveCheck.glade')
+ 
+        self.txtUsername = glade.get_widget('txtUsername')
+        self.txtPassword = glade.get_widget('txtPassword')
+        self.btnLogin = glade.get_widget('btnLogin')
+        self.window = glade.get_widget('mainWindow')
+ 
+        menu = gtk.Menu() 
+        menuItem = gtk.ImageMenuItem(gtk.STOCK_QUIT) 
+        menuItem.connect('activate', lambda q: gtk.main_quit()) 
+        menu.append(menuItem) 
+ 
+        self.statusIcon = gtk.status_icon_new_from_file('wave.png')
+        self.statusIcon.set_visible(True)
+ 
+        self.statusIcon.connect('activate', self.onIconClick)
+        self.statusIcon.connect('popup-menu', self.showPopupMenu, menu)
+        self.window.connect('delete_event', lambda q: gtk.main_quit())
+ 
+        glade.signal_autoconnect(self)
+ 
+    def main(self):
+        gtk.main()
+ 
 if __name__ == '__main__':
     main = WaveChecker()
+    main.main()
